35d6b8e3e45a8f075449e94ede27091e2c9f7dbe
GeoCarter96 <geolcarter21@gmail.com>
2025-11-29 21:35:26 -0500

    add

--- index.js
+++ index.js
@@ -10,13 +10,18 @@ async function main() {
 
 main();
 
+function showUserPosts(id) {
+    localStorage.setItem("id",id);
+    window.location.href = `${window.location.origin}/user.html`
+}
+
 function userHtml(user) {
-    ` <div class="user-card">
+   return ` <div class="user-card" onclick="showUserPosts(${user.id})">
                 <div class="user-card__container">
                     <h3>${user.name}</h3>
                     <p><b>Email:</b>${user.email}</p>
                     <p><b>Phone:</b> ${user.phone}</p>
                     <p><b>Website:</b> <a href="https://${user.website}" target="_blank">${user.website}</a></p>
                 </div>
-                </div>`
+                </div>`;
 }
